bf4524cce92bc9de16ff5d9ad665f08c142a6dd6
Nirmal Agarwal <nirmal.agarwal@oracle.com>
2020-07-28 23:30:38 +0100

    Performance improved

--- eq/new_approach.py
+++ eq/new_approach.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import csv
+import logging
 import sys
 import string
 from copy import deepcopy
 import difflib
 import argparse
+from itertools import groupby
 
 SCHEME_CSV="schemes.csv"
-
-POSSIBLE_PLAN = ['growth', 'dividend']
 
 def read_csv(csvfile):
     all_rows = []
@@ -22,28 +22,56 @@ def read_csv(csvfile):
                 all_rows.append(row)
     return all_rows
 
+def group_mfapi_data(all_mfapi):
+    '''
+    Take a list of dictionaries where each dictionary represents a scheme.
+    Convert it into a dictionary of dictionaries grouped by fund house.
+    dict[fund_house] = [ list of schemes by fund house ]
+    '''
+    # First sort list by fund house name
+    sorted_mfapi=sorted(all_mfapi, key=lambda x: x['fund_house'])
+
+    # now groupthem into dictionary with key as fund house name and value as
+    # list of schemes
+    grouped_data = {k.lower() : list(v) for k,v in groupby(sorted_mfapi,
+                                                   key=lambda x: x['fund_house'])}
+    return grouped_data
 
 def main(pdfcsv, all_match):
     my_tx = read_csv(pdfcsv)
 
     schmes = read_csv(SCHEME_CSV)
 
-    POSSIBLE_VALES = [ d['scheme_name'].lower().replace('.', '')
-                      for d in schmes ]
+    # arrange data
+    grouped_data = group_mfapi_data(schmes)
+
+
+    '''
+    # scheme name in mfuapi data is of this structure
+    # DSP Tax Saver Fund - Regular Plan - Growth
+    # SBI HEALTHCARE OPPORTUNITIES FUND - REGULAR PLAN -GROWTH
+    # Canara Robeco Equity Taxsaver Fund - Regular Plan - Dividend
+    # Canara Robeco Equity Taxsaver Fund - Regular Plan - Growth
+    # Canara Robeco Equity Taxsaver Fund - Direct Plan - Growth Option
+    # Canara Robeco Equity Taxsaver Fund - Direct Plan - Dividend Option
+    UTI-Dividend Yield Fund.-Income
+    UTI-Dividend Yield Fund.-Growth
+    UTI-Dividend Yield Fund.-Growth-Direct
+    UTI-Dividend Yield Fund.-Income-Direct
+    Mirae Asset Emerging Bluechip Fund - Regular Plan - Dividend
+Mirae Asset Emerging Bluechip Fund - Regular Plan - Growth Option
+Mirae Asset Emerging Bluechip Fund - Direct Plan - Growth
+Mirae Asset Emerging Bluechip Fund - Direct Plan - Dividend
+    '''
+    # { Namme of scheme } {Regular/Direct}  {Growth/Dividend/Bonus}
 
     # set of unique fund name from PDF which are slightly filtered as below
     unique_modified = set()
-    # Set of unique fund names from PDF as it is
-    unique_unmodified = set()
 
     for entry in my_tx:
-        # Just to compare how we get results
-        unique_unmodified.add(entry['Fund'])
         all_parts = entry['Fund'].split('-')
-        #import pdb;pdb.set_trace()
         # doing some processing to the fundname from PDF
         fund = ' '.join(all_parts[1:])
-
         name = fund.strip() # remove trailing spaces
         name = name.lstrip(string.digits).strip() # remove trailing spaces
         # remove anything after '(' Mostly contains old scheme name or advisor
@@ -52,22 +80,35 @@ def main(pdfcsv, all_match):
         name = ' '.join(name.split())
         unique_modified.add(name)
 
-    print("$$ USING modified fund names from PDF Against scheme.csv")
+    logging.debug(" USING modified fund names from PDF Against scheme.csv")
     for mf in unique_modified:
-        print(f' #### Finding match for {mf} ####')
-        diff_ret = difflib.get_close_matches(mf.lower(), POSSIBLE_VALES)
+        print(f' {mf}')
+        continue
+        #print(f' #### Finding match for {mf} ####')
+        # match fund house first
+        search_list = []
+        possible_fund_house = mf.split(' ')[0].lower()
+        for house in grouped_data:
+            if possible_fund_house in house:
+                search_list = [ d['scheme_name'].lower().replace('.', '')
+                               for d in grouped_data[house]]
+                break
+        diff_ret = difflib.get_close_matches(mf.lower(), search_list)
         if not len(diff_ret):
-            print('No match found')
+            print("NOT FOUND")
             continue
         if all_match:
-            print(f'\t {diff_ret}')
+            for ret in diff_ret:
+                print(f'\t {ret}')
         else:
             print(f'\t {diff_ret[0]}')
-    sys.exit()
 
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='debug-match.log', filemode='w',
+                        format='%(name)s - %(levelname)s - %(message)s',
+                        level=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--input-file",
                         help ="CSV file from PDF convertor",
